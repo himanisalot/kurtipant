9feabca38429a083d0a0c482a462c541c95b6aef
Himani <creativefashionlovers@gmail.com>
2026-06-21 10:56:30 +0530

    Add env var diagnostic

--- api/test-ping.js
+++
@@ -1,9 +0,0 @@
-// api/test-ping.js
-//
-// Minimal diagnostic function with zero external dependencies.
-// If this doesn't respond, the problem is in platform/deploy config,
-// not in the razorpay/googleapis/nodemailer code.
-
-module.exports = (req, res) => {
-  res.status(200).json({ ok: true, message: 'pong', time: new Date().toISOString() });
-};
